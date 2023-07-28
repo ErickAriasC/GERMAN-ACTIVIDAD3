1bd2faab9d0cfc54c3a53b97e4b10ac373e3a743
Erick_Arias <eariascardona0@soy.sena.edu.co>
2023-07-28 10:47:57 -0500

    se agregó el alert() donde muestra el numero promedio

--- promedio.js
+++ promedio.js
@@ -1,2 +1,4 @@
 let number_1=Number(prompt("INGRESE EL PRIMER NUMERO"));
 let number_2=Number(prompt("INGRESE EL SEGUNDO NUMERO"));
+
+alert(`El promedio de los 2 numero ingresados es:\n${(number_1+number_2)/2}`);
